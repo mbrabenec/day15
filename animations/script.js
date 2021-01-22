b87a1192e2b404a27f98b6dc338b2c458896ba7d
mbrabenec <brabzzz@gmail.com>
2021-01-22 12:32:38 +0100

    ul

---
+++ animations/script.js
@@ -0,0 +1,21 @@
+
+
+
+let spinner = document.querySelector(".spinner");
+let pic = document.querySelector(".pic");
+
+setTimeout(() => {
+
+    spinner.classList.toggle("hidden");
+    pic.classList.toggle("hidden");
+
+
+
+}, 3000)
+
+
+
+
+
+
+
